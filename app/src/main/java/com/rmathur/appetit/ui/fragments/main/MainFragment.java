4e1676fee02cc01aef45ed789b3f448a01106f5a
Rohan Mathur <rohan@rmathur.com>
2015-04-08 01:37:59 -0500

    Initial card work
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- app/src/main/java/com/rmathur/appetit/ui/fragments/main/MainFragment.java
+++ app/src/main/java/com/rmathur/appetit/ui/fragments/main/MainFragment.java
@@ -2,15 +2,24 @@ package com.rmathur.appetit.ui.fragments.main;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
+import com.andtinder.model.CardModel;
+import com.andtinder.model.Orientations;
+import com.andtinder.view.CardContainer;
+import com.andtinder.view.SimpleCardStackAdapter;
 import com.rmathur.appetit.R;
 
 public class MainFragment extends Fragment {
+
+    @InjectView(R.id.layoutview)
+    CardContainer mCardContainer;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -19,8 +28,41 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_alerts, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.inject(this, view);
+
+        mCardContainer = (CardContainer) view.findViewById(R.id.layoutview);
+        mCardContainer.setOrientation(Orientations.Orientation.Disordered);
+
+        CardModel card = new CardModel("Title1", "Description goes here", this.getActivity().getResources().getDrawable(R.drawable.swipe_icon));
+
+        card.setOnCardDimissedListener(new CardModel.OnCardDimissedListener() {
+            @Override
+            public void onLike() {
+                Log.d("Swipeable Card", "I liked it");
+            }
+
+            @Override
+            public void onDislike() {
+                Log.d("Swipeable Card", "I did not liked it");
+            }
+        });
+
+        card.setOnClickListener(new CardModel.OnClickListener() {
+            @Override
+            public void OnClickListener() {
+                Log.i("Swipeable Cards","I am pressing the card");
+            }
+        });
+
+        SimpleCardStackAdapter adapter = new SimpleCardStackAdapter(this.getActivity());
+        adapter.add(card);
+        adapter.add(card);
+        adapter.add(card);
+        adapter.add(card);
+        adapter.add(card);
+        adapter.add(card);
+        mCardContainer.setAdapter(adapter);
 
         return view;
     }
